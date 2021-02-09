7b58290047bba5209cc0b08ea9945c4907df3ee4
mbenetti <mbenetti@hotmail.es>
2021-02-09 23:04:27 +0100

    first commit

---
+++ myscript.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Primero iniciar el DS docker en la carpeta que contenga los datos
+# docker run --rm -it -v ${PWD}:/data datascienceworkshops/data-science-at-the-command-line
+
+# echo 1- To show the working directory: $ pwd
+# pwd
+
+
